78f6b2ffa4bed32e716ca05ede43ae3bdb02e1a5
ankur-upflairs <ankur.upflairs@gmail.com>
2024-07-16 18:06:09 +0530

    strings

---
+++ js/number and string/Number.js
@@ -0,0 +1,17 @@
+//number=> integer and float
+let a=12
+let b=23.1278
+// console.log(Number.MAX_SAFE_INTEGER)
+// console.log(Number.MAX_VALUE)
+// console.log(Number.NaN)
+// console.log('a'/12)
+console.log(undefined+12)
+// console.log(Number.isInteger(12.3))
+// console.log(Number.isFinite(Number.POSITIVE_INFINITY))
+// console.log(typeof Number('23'))
+// console.log(b.toExponential())
+// console.log(b.toFixed(2))
+// console.log(b.toPrecision(5))
+
+
+
---
+++ js/number and string/string.js
@@ -0,0 +1,60 @@
+//string => collection of characters
+//strings are immutable in js
+//can be written in '' or ""
+let a='hello'
+let b="hello"
+//template string or string literanl
+let c=`hello my name is ankit`
+//multiline text
+let d=`hello 
+my name is ankit`
+//variable can be inserted dynamically
+let age=34
+let e=`${a} my age is ${age}`
+console.log(e)
+//String method =>
+//indexing is the way to access character in a string
+//indexing starts with zero 0
+//increases one by one
+// console.log(a[4])
+// console.log(d[4])
+//length=> total no. of characters
+// console.log(b.length)
+// console.log(c.length)
+// console.log(a.charAt(2))
+// console.log(a.endsWith('loo'))
+// console.log(a.startsWith('l'))
+// console.log(a.includes('ellx'))
+// console.log(a.indexOf('l'))
+// console.log(a.lastIndexOf('l'))
+// console.log(a.padEnd(10,'8'))
+// console.log(a.padStart(15,'$'))
+// console.log(a.repeat(4))
+let f=a.replace('e','x')
+console.log(f)
+console.log(a)
+// console.log(a.replaceAll('l','z'))
+// console.log(a.concat(' ankit'))
+// console.log(a.concat(b))
+// console.log(a+b)
+// console.log(d.split(''))//convert string to array
+// console.log(a.slice(2,4))
+// console.log(a.slice(3))
+// console.log(c.slice(17))
+// console.log(c.slice(6,8))
+let g="        ankit       "
+// console.log(g.trimStart())
+// console.log(g.trimEnd())
+// console.log(g.trim())
+// console.log(typeof String(12))
+// let i=45
+// console.log(typeof i.toString())
+// console.log(typeof (""+i))
+// console.log(a.toUpperCase())
+// console.log(c.toLowerCase())
+// console.log()
+
+
+
+
+
